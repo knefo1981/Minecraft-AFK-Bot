3ede09ce10c795f0fdd57e30581070b6b0c4ef6e
knefo1981 <knefo1981@gmail.com>
2026-06-29 22:51:57 +0300

    for bot.js

--- bot.js
+++ bot.js
@@ -2,8 +2,8 @@ const mineflayer = require('mineflayer');
 const config = require('./config.json');
 
 const bot = mineflayer.createBot({
-  host: config.serverHost,
-  port: config.serverPort,
+  host: config.ns566750.seedloaf.com:49385,
+  port: config.49385,
   username: config.botUsername,
   auth: 'offline',
   version: false,
